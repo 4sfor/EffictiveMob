3e187cea0aaa3882c1fc6cfda49262ad46b41785
4sfor <90134074+4sfor@users.noreply.github.com>
2024-11-20 08:34:27 +0400

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Patch } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('problem/')
+@Controller('problem')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
